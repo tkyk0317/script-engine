1bbbec496bee4c2c1a6f785384a9b00fb7c7152d
takayuki <takayuki.1981.0317@gmail.com>
2017-09-24 15:58:07 +0900

    [add] add chapter4.
    第四章までのコードを追加.

---
+++ src/main/java/lexer/stone/ast/BinaryExpr.java
@@ -0,0 +1,10 @@
+package stone.ast;
+
+import java.util.List;
+
+public class BinaryExpr extends ASTList {
+    public BinaryExpr(List<ASTree> c) { super(c); }
+    public ASTree left() { return child(0); }
+    public ASTree right() { return child(2); }
+    public String operator() { return ((ASTLeaf)child(1)).token().getText(); }
+}
